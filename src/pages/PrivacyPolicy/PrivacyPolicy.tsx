3d535fac6db3bd16e291cf07383ad8aa09ec7c06
drakata27 <theall3xander@gmail.com>
2025-04-10 19:03:20 +0100

    added htaccess file

--- src/pages/PrivacyPolicy/PrivacyPolicy.tsx
+++ src/pages/PrivacyPolicy/PrivacyPolicy.tsx
@@ -1,4 +1,4 @@
-// import ReactMarkdown from "react-markdown";
+import ReactMarkdown from "react-markdown";
 import privacy from "../../data/privacy";
 
 const PrivacyPolicy = () => {
@@ -6,8 +6,7 @@ const PrivacyPolicy = () => {
     <div className="ml-[20px] mr-[20px] sm:ml-[30px] sm:mr-[30px] md:ml-[100px] md:mr-[100px] lg:ml-[200px] lg:mr-[200px] xl:ml-[300px] xl:mr-[300px]">
       <h1 className="text-4xl font-bold pb-5">Privacy Policy</h1>
       <div className="prose space-y-5">
-        {/* <ReactMarkdown>{privacy}</ReactMarkdown> */}
-        <p>{privacy}</p>
+        <ReactMarkdown>{privacy}</ReactMarkdown>
       </div>
     </div>
   );
