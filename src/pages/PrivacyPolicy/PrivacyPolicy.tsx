5da53d5439404007019607685b51baf6db80633f
drakata27 <theall3xander@gmail.com>
2025-04-10 19:16:20 +0100

    added vercel json and refactored

--- src/pages/PrivacyPolicy/PrivacyPolicy.tsx
+++ src/pages/PrivacyPolicy/PrivacyPolicy.tsx
@@ -1,7 +1,16 @@
+import { useEffect, useState } from "react";
 import ReactMarkdown from "react-markdown";
-import privacy from "../../data/privacy";
 
 const PrivacyPolicy = () => {
+  const [privacy, setPrivacy] = useState("");
+
+  useEffect(() => {
+    fetch("/privacy.md")
+      .then((res) => res.text())
+      .then((text) => setPrivacy(text))
+      .catch((err) => console.error("Error loading markdown:", err));
+  }, []);
+
   return (
     <div className="ml-[20px] mr-[20px] sm:ml-[30px] sm:mr-[30px] md:ml-[100px] md:mr-[100px] lg:ml-[200px] lg:mr-[200px] xl:ml-[300px] xl:mr-[300px]">
       <h1 className="text-4xl font-bold pb-5">Privacy Policy</h1>
